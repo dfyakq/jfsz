6a1f696e74c9896c68ab35d46fd6f042d45c2404
junyou.an <junyou.an@maitang001.com>
2025-09-24 12:30:20 +0800

    x

--- docs/.vuepress/.temp/internal/routes.js
+++
@@ -1,7 +0,0 @@
-export const redirects = JSON.parse("{}")
-
-export const routes = Object.fromEntries([
-  ["/", { loader: () => import(/* webpackChunkName: "index.html" */"/Users/skg/CodeProjects/jfsz/docs/.vuepress/.temp/pages/index.html.js"), meta: {"title":"Home"} }],
-  ["/get-started.html", { loader: () => import(/* webpackChunkName: "get-started.html" */"/Users/skg/CodeProjects/jfsz/docs/.vuepress/.temp/pages/get-started.html.js"), meta: {"title":"Get Started"} }],
-  ["/404.html", { loader: () => import(/* webpackChunkName: "404.html" */"/Users/skg/CodeProjects/jfsz/docs/.vuepress/.temp/pages/404.html.js"), meta: {"title":""} }],
-]);
--- docs/get-started.md
+++ docs/get-started.md
@@ -4,7 +4,7 @@ This is a normal page, which contains VuePress basics.
 
 ## Pages
 
-You can add markdown files in your vuepress directory, every markdown file will be converted to a page in your site.
+You can add markdown files in your vuepress directory, every markdown file will be converted to a page in your site. 
 
 See [routing][] for more details.
 
